886871e96fca9bf2450371c2ff0f3171b36f7221
Hernando Cartagena <hernando.cartagenasaldivar@telefonica.com>
2022-12-02 17:09:29 -0500

    Initial commit

---
+++ app/src/main/java/com/example/transporte/ui/fragments/ViajesFragment.kt
@@ -0,0 +1,23 @@
+package com.example.transporte.ui.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.transporte.R
+
+
+class ViajesFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        val view: View =  inflater.inflate(R.layout.fragment_viajes, container, false)
+
+            return view
+    }
+
+}
